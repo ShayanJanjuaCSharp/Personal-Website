f4d0de3674c7540482d0411dcdfd1d0b26905757
Shayan Janjua <shayan.janjua@city.ac.uk>
2025-01-27 13:38:29 +0000

    r3f is being stupid with deoendency issues, probably

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Canvas } from "@react-three/fiber";
+
+export default function Home() {
+  return (
+    <Canvas>
+
+    </Canvas>
+  );
+}
