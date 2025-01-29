04fe4107022fe250a15d7935b507808466031c6b
Shayan Janjua <shayanjanjua.london@gmail.com>
2025-01-29 20:14:23 +0000

    Got some sort of progress. this stuff is dificult.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Projects from "./pages/Projects.jsx";
+import Skills from "./pages/Skills.jsx";
+import Hobbies from "./pages/Hobbies.jsx";
+import Contact from "./pages/Contact.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/skills",
+    element: <Skills />,
+  },
+  {
+    path: "/hobbies",
+    element: <Hobbies />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Suspense>
+      <RouterProvider router={router} />
+    </Suspense>
+  </StrictMode>
+);
