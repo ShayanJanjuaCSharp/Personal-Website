2b19bdc1376a920120edc460cb247beeb5989f4a
Shayan Janjua <shayanjanjua.london@gmail.com>
2025-02-13 18:12:42 +0000

    Added cc license in prep for model import.

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,12 @@ import { useFont } from "@react-three/drei";
 import { useEffect, useState } from "react";
 import "./App.css";
 import { use } from "react";
+import { color } from "three/tsl";
 
 export default function App() {
   const [intro, setIntro] = useState("");
+  const tubeCredds =
+    "<a href='https://skfb.ly/ossIt'>Bombardier S Stock London Underground</a> by timblewee is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).";
   const [wi, setwi] = useState(
     (90 * (window.innerWidth - 100)) / (screen.width - 100) + 15
   );
@@ -91,8 +94,29 @@ export default function App() {
           width: "100vw",
           height: "100vh",
           zIndex: 0,
-          pointerEvents: "none",
         }}>
+        <p
+          style={{
+            position: "absolute",
+            zIndex: 0,
+            color: "grey",
+            fontSize: "1.5vw",
+            bottom: 0,
+            right: "1%",
+            textAlign: "right",
+            width: "30vw",
+          }}>
+          <a href="https://skfb.ly/ossIt">
+            Bombardier S Stock London Underground
+          </a>
+          {" by "}
+          <a href="https://sketchfab.com/timblewee">timblewee</a> is licensed
+          under
+          <a href="http://creativecommons.org/licenses/by/4.0/">
+            {" Creative Commons Attribution"}
+          </a>
+          .
+        </p>
         <div
           className="move"
           style={{
@@ -106,6 +130,7 @@ export default function App() {
             alignContent: "center",
             display: "grid",
             opacity: 1,
+            pointerEvents: "none",
           }}>
           <div
             style={{
@@ -150,7 +175,7 @@ export default function App() {
           height: "5vh",
           top: "3%",
           right: "0",
-          //pointerEvents: "none",
+          pointerEvents: "none",
           opacity: 0,
         }}>
         <button
