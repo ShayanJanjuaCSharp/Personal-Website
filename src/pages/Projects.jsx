9ab3836fe11cb71499fefb167ccd9553c3134383
Shayan Janjua <shayanjanjua.london@gmail.com>
2025-03-03 16:08:59 +0000

    Got final models and leadenhall market surroundings

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,4 @@
-import { Text } from "@react-three/drei";
+import { Fisheye, Text } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 import { Gltf } from "@react-three/drei";
@@ -8,20 +8,31 @@ export default function Projects(props) {
     <>
       <hemisphereLight intensity={0.5} color="white" groundColor="black" />
       <Environment
-        files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/evening_road_01_2k.hdr"
+        files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/leadenhall_market_4k.hdr"
         ground={{ height: 5, radius: 40, scale: 20 }}
       />
-      <Gltf src={"./models/ps1.glb"} position={[-1.7, -2, -1.5]} scale={0.4} />
       <Gltf
-        src={"./models/1971.glb"}
-        position={[-0.6, -2, -1.5]}
-        scale={0.29}
+        src={"./models/ps1.glb"}
+        position={[-1.7, -2, -1.5]}
+        scale={0.4}
+        rotation={[0, Math.PI / 4, 0]}
+        onClick={() => console.log("ps1")}
       />
       <Gltf
-        src={"./models/atm.glb"}
+        src={"./models/hl2crate.glb"}
+        position={[0, -1.75, -1.5]}
+        scale={1.7}
+        rotation={[0, Math.PI / 4, 0]}
+        onClick={() => console.log("hl2crate")}
+        onPointerEnter={(e) => (e.object.material.emissive = "hotpink")}
+        onPointerLeave={(e) => (e.object.material.emissive = "none")}
+      />
+      <Gltf
+        src={"./models/C2.glb"}
         position={[1.8, -2, -1.5]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={0.35}
+        rotation={[0, Math.PI / 4, 0]}
+        scale={1.5}
+        onClick={() => console.log("C2")}
       />
     </>
   );
