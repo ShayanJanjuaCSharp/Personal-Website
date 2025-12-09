ca4aff53863446188ecd3121f0fe9f8c6a37f72e
Shayan Janjua <153615119+ShayanJanjuaCSharp@users.noreply.github.com>
2025-12-09 12:39:08 +0000

    Link to alternative accessible website.

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -4,6 +4,8 @@ export default function Loading() {
     <div className="stage">
       <div className="text">
         <h3 className="load">Loading...</h3>
+        <p>If the page is taking long to load, please click the link below</p>
+        <a href="https//shayanjanjuapw.vercel.app"></a>
       </div>
     </div>
   );
